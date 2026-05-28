651dbaabd913abfa7c9872067e6f5cb0a0b76be1
HamzaFlexOps <hkkarabala@flex-op.com>
2026-05-28 13:22:54 +0300

    Add project files.

---
+++ Smart Training Institute Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Smart_Training_Institute_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
